3249af70b46abc675e15854be67ed72655b68573
BlairLi <1579193739@qq.com>
2024-06-17 10:40:52 -0400

    run python voice analysis algorithm and react native front end at the same time

--- app/navigation/LessonNavigator.jsx
+++ app/navigation/LessonNavigator.jsx
@@ -4,6 +4,14 @@ import LessonsScreen from "../screens/Lessons/LessonsScreen";
 import LessonDetailScreen from "../screens/Lessons/LessonDetailScreen";
 import BasicScreen from "../screens/Lessons/BasicScreen";
 import PitchScreen from "../screens/Lessons/PitchScreen";
+// import InotnationScreen from "../screens/Lessons/InotnationScreen";
+// import ResonanceScreen from "../screens/Lessons/ResonanceScreen";
+// import PuttingtheBascisTogetherScreen from "../screens/Lessons/PuttingtheBascisTogetherScreen";
+// import AdvancedPitchModuleScreen from "../screens/Lessons/AdvancedPitchModuleScreen";
+// import VolumeScreen from "../screens/Lessons/VolumeScreen";
+// import ArticulationScreen from "../screens/Lessons/ArticulationScreen";
+// import SyntaxScreen from "../screens/Lessons/SyntaxScreen";
+// import PuttingEverythingTogetherScreen from "../screens/Lessons/PuttingEverythingTogetherScreen";
 import HomeworkScreen from "../screens/Lessons/HomeworkScreen";
 import HomeworkDetailScreen from "../screens/Lessons/HomeworkDetailScreen";
 import NotificationsScreen from "../screens/Settings/NotificationsScreen";
@@ -31,6 +39,15 @@ export default function LessonNavigator() {
         <Stack.Screen name="Introduction" component={LessonDetailScreen} />
         <Stack.Screen name="Basics" component={BasicScreen} />
         <Stack.Screen name="Pitch" component={PitchScreen} />
+        {/* TODO */}
+        {/* <Stack.Screen name="Inotnation" component={InotnationScreen} />
+        <Stack.Screen name="Resonance" component={ResonanceScreen} />
+        <Stack.Screen name="Putting the Bascis Together" component={PuttingtheBascisTogetherScreen} />
+        <Stack.Screen name="Advanced Pitch Module" component={AdvancedPitchModuleScreen} />
+        <Stack.Screen name="Volume" component={VolumeScreen} />
+        <Stack.Screen name="Articulation" component={ArticulationScreen} />
+        <Stack.Screen name="Syntax" component={SyntaxScreen} />
+        <Stack.Screen name="Putting Everything Together" component={PuttingEverythingTogetherScreen} /> */}
         <Stack.Screen name="Homework" component={HomeworkScreen} />
         <Stack.Screen name="HomeworkDetail" component={HomeworkDetailScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
